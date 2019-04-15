bb160852dd07f87819abedd430f915a148542312
pawkulka <pawkulka@LNAR-JYK6MC2.corp.capgemini.com>
2019-04-14 22:47:27 -0500

    Added sorting project

---
+++ sorting/src/main/java/algo/sorting/SelectionSort.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package algo.sorting;
+
+/**
+ * @author pawkulka
+ *
+ */
+public class SelectionSort {
+	
+	public void sort(int[] nums) {
+		if(nums != null && nums.length > 1) {
+			int minIndex;
+			for(int i=0; i < nums.length - 1; i++) {
+				minIndex = i;
+				for(int j=i+1; j < nums.length; j++) {
+					if(nums[j] < nums[minIndex])
+						minIndex = j;
+				}
+				
+				if(minIndex != i) {
+					int temp = nums[i];
+					nums[i] = nums[minIndex];
+					nums[minIndex] = temp;
+				}
+			}
+			
+		}
+	}
+
+}
